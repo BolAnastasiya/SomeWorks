7b89b96975b0f0a1e0239c0c174e26a2cdb464d1
BolAnastasiya <125025551+BolAnastasiya@users.noreply.github.com>
2024-03-15 18:25:10 +0300

    Add files via upload

---
+++ C#/Lab1/Stilizasia/AddEditPage.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Stilizasia
+{
+    /// <summary>
+    /// Логика взаимодействия для AddEditPage.xaml
+    /// </summary>
+    public partial class AddEditPage : Page
+    {
+        private Hotel _currentHotel = new Hotel();
+
+        public AddEditPage(Hotel selectedHotel)
+        {
+            InitializeComponent();
+            ComboCountries.ItemsSource = HotelsEntities.GetContext().Country.ToList();
+            if(selectedHotel != null)
+                _currentHotel = selectedHotel;
+
+            DataContext = selectedHotel;
+        }
+
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder errors = new StringBuilder();
+            if(string.IsNullOrWhiteSpace(_currentHotel.Name_hotel))
+            {
+                errors.AppendLine("Укажите название отеля");
+            }
+            if(_currentHotel.Count_of_stars < 1 || _currentHotel.Count_of_stars > 5)
+            {
+                errors.AppendLine("Количество звёзд - от 1 до 5");
+            }
+            if(_currentHotel.Country == null)
+            {
+                errors.AppendLine("Выберите страну");
+            }
+            if(errors.Length > 0)
+            {
+                MessageBox.Show(errors.ToString());
+                return;
+            }
+            if(_currentHotel.id == 0)
+            {
+                HotelsEntities.GetContext().Hotel.Add(_currentHotel);
+            }
+            try
+            {
+                HotelsEntities.GetContext().SaveChanges();
+                MessageBox.Show("Информация сохранена");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+
+    }
+}
---
+++ C#/Lab1/Stilizasia/HotelPage.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Stilizasia
+{
+    /// <summary>
+    /// Логика взаимодействия для HotelPage.xaml
+    /// </summary>
+    public partial class HotelPage : Page
+    {
+        public HotelPage()
+        {
+            InitializeComponent();
+            DGridHotels.ItemsSource = HotelsEntities.GetContext().Hotel.ToList();
+        }
+
+        private void BtnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPage((sender as Button).DataContext as Hotel));
+        }
+
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPage(null));
+        }
+
+        private void BtnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            var hotelsForRemoving = DGridHotels.SelectedItems.Cast<Hotel>().ToList();
+            if(MessageBox.Show($"Вы точно хотите удалить следующие {hotelsForRemoving.Count()} элементов?", "Внимание", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                try
+                {
+                    HotelsEntities.GetContext().Hotel.RemoveRange(hotelsForRemoving);
+                    HotelsEntities.GetContext().SaveChanges();
+                    MessageBox.Show("Данные удалены!");
+
+                    DGridHotels.ItemsSource = HotelsEntities.GetContext().Hotel.ToList();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+            }
+        }
+
+        private void Page_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (Visibility == Visibility.Visible)
+            {
+                HotelsEntities.GetContext().ChangeTracker.Entries().ToList().ForEach(p => p.Reload());
+                DGridHotels.ItemsSource = HotelsEntities.GetContext().Hotel.ToList();
+            }
+        }
+    }
+}
